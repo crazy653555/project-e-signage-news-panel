ad23a1e567af33b3f5f13d44f87b9d8cb13a8d86
meyu <yrrvknone@gmail.com>
2018-02-28 07:10:37 +0800

    規格藍圖

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ClockComponent } from './clock/clock.component';
+import { NowComponent } from './weather/now/now.component';
+import { WeekComponent } from './weather/week/week.component';
+import { CityComponent } from './weather/city/city.component';
+import { NewsComponent } from './news/news.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClockComponent,
+    NowComponent,
+    WeekComponent,
+    CityComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/weather/now/now.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-now',
+  templateUrl: './now.component.html',
+  styleUrls: ['./now.component.css']
+})
+export class NowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
