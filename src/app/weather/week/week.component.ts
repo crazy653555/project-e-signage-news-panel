e301076d0f00d4f06269a5193ea14588f9ff3cea
eason <crazy653555@gmail.com>
2018-03-06 20:02:50 +0800

    新增華氏轉攝氏pipe

--- src/app/weather/week/week.component.ts
+++ src/app/weather/week/week.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from "@angular/core";
-import { Weather2Service } from "../weather2.service";
-import { Daily } from "../daily";
+import { Component, OnInit } from '@angular/core';
+import { Weather2Service } from '../weather2.service';
+import { Daily } from '../daily';
 
 @Component({
-  selector: "app-week",
-  templateUrl: "./week.component.html",
-  styleUrls: ["./week.component.css"]
+  selector: 'app-week',
+  templateUrl: './week.component.html',
+  styleUrls: ['./week.component.css']
 })
 export class WeekComponent implements OnInit {
   weatherData: any;
 
-  constructor(private weather: Weather2Service) {}
+  constructor(private weather: Weather2Service) { }
 
   ngOnInit() {
     this.weather.getWeekWeather().subscribe(data => (this.weatherData = data));
